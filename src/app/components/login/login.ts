27f350d9bf10700cf0da6ab198e845cecb9c6ce0
Rakesh8086 <rakeshteja7007@gmail.com>
2025-12-15 23:34:24 +0530

    Login functionality implemented

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../_services/auth.service';
+import { StorageService } from '../../_services/storage.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrls: ['./login.css'],
+  standalone: true,
+  imports: [CommonModule, FormsModule]
 })
-export class Login {
+export class Login implements OnInit {
+  form: any = {
+    username: null,
+    password: null
+  };
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  roles: string[] = [];
 
+  constructor(private authService: AuthService, private storageService: StorageService) { }
+
+  ngOnInit(): void {
+    if (this.storageService.isLoggedIn()) {
+      this.isLoggedIn = true;
+      this.roles = this.storageService.getUser().roles;
+    }
+  }
+
+  onSubmit(): void {
+    const username = this.form.username;
+    const password = this.form.password;
+
+    this.authService.login(username, password).subscribe({
+      next: data => {
+        this.storageService.saveUser(data);
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.roles = this.storageService.getUser().roles;
+        this.reloadPage();
+      },
+      error: err => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      }
+    });
+  }
+
+  reloadPage(): void {
+    window.location.reload();
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient} from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { authInterceptorProviders } from './app/_helpers/auth.interceptor';
 import { routes } from './app/app-routing.module'; // Import the routes array
@@ -8,9 +8,7 @@ import { routes } from './app/app-routing.module'; // Import the routes array
 bootstrapApplication(App, {
   providers: [
     provideRouter(routes),     
-    provideHttpClient(
-      withInterceptors([/* Add function interceptors here if needed */])
-    ),
-    authInterceptorProviders[0] 
+    provideHttpClient(),
+    ...authInterceptorProviders
   ]
 }).catch(err => console.error(err));
