b00eb4247267b1b04386fa11ea3a7311aadeff9f
Rakesh8086 <rakeshteja7007@gmail.com>
2025-12-15 23:35:21 +0530

    Switched from JWT cookie to JWT bearer token based authentication

--- src/app/_helpers/auth.interceptor.ts
+++ src/app/_helpers/auth.interceptor.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { StorageService } from '../_services/storage.service';
+
+const TOKEN_HEADER_KEY = 'Authorization';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor() {
+    constructor(private storageService: StorageService) { 
 
-  }
+    } 
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const authReq = req.clone({
-      withCredentials: true, // sends JWT stored in cookie
-    });
-    
-    return next.handle(authReq);
-  }
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        let authReq = req;
+        const user = this.storageService.getUser();
+        if(user != null && user.token) { 
+            authReq = req.clone({ 
+                headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + user.token) 
+            });
+        }
+        return next.handle(authReq);
+    }
 }
 
 export const authInterceptorProviders = [
