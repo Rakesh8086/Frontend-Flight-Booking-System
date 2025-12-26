f9c3b1c7d2888c83ec7b1bb9e921d76731d94a7b
Rakesh8086 <rakeshteja7007@gmail.com>
2025-12-26 18:01:12 +0530

    Session(JWT) expiry implemented by removing logged in user from session storage

--- src/app/_helpers/http.interceptor.ts
+++ src/app/_helpers/http.interceptor.ts
@@ -1,18 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS, HttpErrorResponse } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 @Injectable()
 export class HttpRequestInterceptor implements HttpInterceptor {
+  constructor(private router: Router) {
+
+  } 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     req = req.clone({
       withCredentials: true,
     });
-
-    return next.handle(req);
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if(error.status === 401) {
+          console.warn("JWT Expired or Unauthorized.");
+          sessionStorage.removeItem('auth-user'); 
+          this.router.navigate(['/login']);
+        }
+        
+        return throwError(() => error);
+      })
+    );
   }
 }
-
 export const httpInterceptorProviders = [
   { provide: HTTP_INTERCEPTORS, useClass: HttpRequestInterceptor, multi: true },
 ];
